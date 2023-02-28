4e679a3e0c2c732ec37da1648b4a13f4a6597d93
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-02-28 13:31:34 +0800

    ok

--- python_crash_course/chp10/p10_1.py
+++ python_crash_course/chp10/p10_1.py
@@ -1,5 +1,5 @@
 
-f_name = 'C:\\Users\\Admin\\OneDrive\\Desktop\\python_basic\\chp10\\learning_python.txt'
+f_name = 'C:\\Users\\Admin\\OneDrive\\Desktop\\教育\\自学\\计算机专业\\编程语言\\python\\python_grammar\\python_master\\python_crash_course\\chp10\learning_python.txt'
 
 with open(f_name) as f_name1:
     print("_____'1'____")
