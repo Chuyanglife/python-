7c0b37d9e4a1685844e45e85de276d6451cc4478
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2023-03-01 21:24:47 +0800

    Update ttestt.py

--- automate_python/chp1/ttestt.py
+++ automate_python/chp1/ttestt.py
@@ -1,7 +1,10 @@
 
 import os
-
-print("Running")
-m=input("pukidad:")
-print(m)
-#os.system("pause")
+j=0
+for i in range(10):
+    print("Running")
+    m=input("pukidad:")
+    print(m)
+    j+=1;
+    #os.system("pause")
+print(j)
